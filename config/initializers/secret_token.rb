3cabaca525b7afa24668a5ef8cbc387df37aa24a
hiro-su <h.sugipon@gmail.com>
2012-12-26 16:15:59 +0900

    hoge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoge::Application.config.secret_token = 'd619eec181964a83583de527c050f4265d552457e6f7c88015634f3b1e2bb6d444bb4407c68991d0a2097c399e01b7a8981d03136f85fbf540afe89b93e4e097'
